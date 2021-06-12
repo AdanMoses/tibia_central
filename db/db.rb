b033af6c6b1ef6981fe1cdf3c14cf1f75fc086fc
Adan Moses <adanmoses@pm.me>
2021-06-12 17:48:52 +1000

    changed pg connect

--- db/db.rb
+++ db/db.rb
@@ -1,6 +1,6 @@
 def run_sql(sql_query, params = [])
 
-  conenction = PG.connect(dbname: 'tibia_app')
+  conenction = PG.connect(ENV['DATABASE_URL'] || {dbname: 'tibia_app'})
   conenction.prepare("statement_name", sql_query)
   results = conenction.exec_prepared("statement_name", params)
   conenction.close
