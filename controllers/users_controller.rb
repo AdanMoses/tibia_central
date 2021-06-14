b301354925d364abbc793a1065b62d4ceb85fbca
Adan Moses <adanmoses@pm.me>
2021-06-14 23:20:06 +1000

    having issues implementing the user edit

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 get '/sign_up' do
   redirect "/users/new"
 end
@@ -28,10 +30,32 @@ get "/user" do
     rejoin = name_split.join "%20"
     character_name = rejoin
   end
-  
+
   result_url = "https://api.tibiadata.com/v2/characters/#{character_name}.json"
 
   response = HTTParty.get(result_url)
 
-  erb :'/users/show', layout: :layout, locals: {results: results, response: response}
+  erb :'/users/show', layout: :layout, locals: {results: results, response: response, userid: userid}
+end
+
+get "/user/:userid/edit" do |userid|
+
+  results = find_user_by_id(userid)
+
+  erb :'users/edit', layout: :layout, locals: { results: results}
+  
+end
+
+put "/user/:userid" do |userid|
+  
+  character_name = params[:character_name]
+  email = params[:email]
+
+  binding.pry
+
+  sql_query = ("UPDATE users SET character_name = $1, email = $2 WHERE userid = $3;")
+  params = [character_name, email, userid]
+  run_sql(sql_query, params)
+
+  redirect "/"
 end
